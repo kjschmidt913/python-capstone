bcf4c9a5f250107a839f25b6df139870ba359c1a
KJ <kj.schmidt913@gmail.com>
2019-05-01 12:20:44 -0400

    updated thank u, next with correct song titles

--- capstone.py
+++ capstone.py
@@ -16,20 +16,18 @@ allData = {'Sweetener': {
     'pete davidson': 'lyrics here',
     'get well soon': 'lyrics here'
 }, 'Thank U, Next': {
-    'raindrops (an angel cried)': ['lyrics here', 'and here'],
-    'blazed': 'lyrics here',
-    'the light is coming': 'lyrics here',
-    'R.E.M.': 'lyrics here',
-    'God is a woman': 'lyrics here',
-    'sweetener': 'lyrics here',
-    'successful': 'lyrics here',
-    'everytime': 'lyrics here',
-    'breathin': 'lyrics here',
-    'no tears left to cry': 'lyrics here',
-    'borderline': 'lyrics here',
-    'goodnight n go': 'lryics here',
-    'pete davidson': 'lyrics here',
-    'get well soon': 'lyrics here'
+    'imagine': ['lyrics here', 'and here'],
+    'needy': 'lyrics here',
+    'NASA': 'lyrics here',
+    'bloodline': 'lyrics here',
+    'fake smile': 'lyrics here',
+    'bad idea': 'lyrics here',
+    'make up': 'lyrics here',
+    'ghostin': 'lyrics here',
+    'in my head': 'lyrics here',
+    '7 rings': 'lyrics here',
+    'thank u, next': 'lyrics here',
+    'break up with your girlfriend, i\'m bored': 'lryics here'
 }
 }
 
