1c96021de1ce49611fdb9ed896087fe1d806ea35
KJ <kj.schmidt913@gmail.com>
2019-05-01 12:07:08 -0400

    still thinking through how to organize the song data in a way that allows me to pull a random lyric

--- capstone.py
+++ capstone.py
@@ -1,9 +1,36 @@
 import random
 
-albums = ["Sweetener", "Thank U, Next"]
+allData = {'Sweetener': {
+    'raindrops (an angel cried)': ['lyrics here', 'and here'],
+    'blazed': 'lyrics here',
+    'the light is coming': 'lyrics here',
+    'R.E.M.': 'lyrics here',
+    'God is a woman': 'lyrics here',
+    'sweetener': 'lyrics here',
+    'successful': 'lyrics here',
+    'everytime': 'lyrics here',
+    'breathin': 'lyrics here',
+    'no tears left to cry': 'lyrics here',
+    'borderline': 'lyrics here',
+    'goodnight n go': 'lryics here',
+    'pete davidson': 'lyrics here',
+    'get well soon': 'lyrics here'
+}, 'Thank U, Next': 'hi'
+}
+
+albums = ['Sweetener', 'Thank U, Next']
+
+
 
 def randomIndex(length):
-    index = random.randint(1,length)
+    index = random.randint(1, length)
     print(index)
 
 randomIndex(400)
+
+def chooseSong():
+    lyric = allData['Sweetener']['blazed']
+    print(lyric)
+
+chooseSong()
+
