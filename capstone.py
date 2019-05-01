61cfcf89bbde738d18b1d649a10f4b99ba5025bf
KJ <kj.schmidt913@gmail.com>
2019-05-01 12:18:21 -0400

    found a way to choose a random dictionary key. can choose a random key in a dict within a dict

--- capstone.py
+++ capstone.py
@@ -15,22 +15,37 @@ allData = {'Sweetener': {
     'goodnight n go': 'lryics here',
     'pete davidson': 'lyrics here',
     'get well soon': 'lyrics here'
-}, 'Thank U, Next': 'hi'
+}, 'Thank U, Next': {
+    'raindrops (an angel cried)': ['lyrics here', 'and here'],
+    'blazed': 'lyrics here',
+    'the light is coming': 'lyrics here',
+    'R.E.M.': 'lyrics here',
+    'God is a woman': 'lyrics here',
+    'sweetener': 'lyrics here',
+    'successful': 'lyrics here',
+    'everytime': 'lyrics here',
+    'breathin': 'lyrics here',
+    'no tears left to cry': 'lyrics here',
+    'borderline': 'lyrics here',
+    'goodnight n go': 'lryics here',
+    'pete davidson': 'lyrics here',
+    'get well soon': 'lyrics here'
 }
-
-albums = ['Sweetener', 'Thank U, Next']
-
+}
 
 
 def randomIndex(length):
     index = random.randint(1, length)
     print(index)
 
+
 randomIndex(400)
 
+
 def chooseSong():
-    lyric = allData['Sweetener']['blazed']
-    print(lyric)
+    albumPick = random.choice(list(allData.keys()))
+    print(albumPick)
+    songPick = random.choice(list(allData[albumPick].keys()))
+    print(songPick)
 
 chooseSong()
-
