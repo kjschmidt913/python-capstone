9a612d8a575c794aa62db5b3341e1dd8ffc7a2f5
KJ <kj.schmidt913@gmail.com>
2019-05-03 12:35:15 -0400

    removed extra code from views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,17 +10,6 @@ def randomIndex(length):
     index = random.randint(0, length)
     return index
 
-def chooseSong():
-    albumPick = random.choice(list(Songs.allData.keys()))
-    songPick = random.choice(list(Songs.allData[albumPick].keys()))
-
-    lyricIndex = randomIndex(len(Songs.allData[albumPick][songPick]) - 2)
-
-    lyrics = Songs.allData[albumPick][songPick][lyricIndex] + \
-        '\n' + Songs.allData[albumPick][songPick][lyricIndex + 1]
-
-    return lyrics
-
 
 def index(request):
     template = loader.get_template('polls/index.html')
@@ -30,7 +19,6 @@ def index(request):
 
     lyricIndex = randomIndex(len(Songs.allData[albumPick][songPick]) - 2)
     context = {
-        'Song': chooseSong(),
         'Lyric_One': Songs.allData[albumPick][songPick][lyricIndex] ,
         'Lyric_Two' : Songs.allData[albumPick][songPick][lyricIndex + 1]
     }
